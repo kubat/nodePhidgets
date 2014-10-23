852add9d8848edcff8869c1d8f76ad92b79dcdf4
Rony Kubat <rony@kubat.org>
2014-10-23 11:06:29 -0400

    Reformat. Cleaner exit on quit. Emit events for attachment.

--- example.js
+++ example.js
@@ -1,18 +1,19 @@
 var phidgetsPrototype = require('./lib/phidgets.js');
 var options = {
-  host: "phidgetsbc.local"
+  host: "localhost"
 }
 var phidgets = new phidgetsPrototype(options);
 
 // events
-phidgets.on('state',  function(state){ console.log("[state] " + state); });
-phidgets.on('error',  function(error){ console.log("[error] " + error); });
-phidgets.on('input',  function(boardId, id, value){ console.log("[" + boardId + "][input]  " + id + " @ " + value); });
-phidgets.on('sensor', function(boardId, id, value){ console.log("[" + boardId + "][sensor] " + id + " @ " + value); });
-phidgets.on('output', function(boardId, id, value){ console.log("[" + boardId + "][output] " + id + " @ " + value); });
+phidgets.on('state',    function(state){ console.log("[state] " + state); });
+phidgets.on('error',    function(error){ console.log("[error] " + error); });
+phidgets.on('input',    function(boardId, id, value){ console.log("[" + boardId + "][input]  " + id + " @ " + value); });
+phidgets.on('sensor',   function(boardId, id, value){ console.log("[" + boardId + "][sensor] " + id + " @ " + value); });
+phidgets.on('output',   function(boardId, id, value){ console.log("[" + boardId + "][output] " + id + " @ " + value); });
+phidgets.on('attached', function(boardId, kind){ console.log("[" + boardId + "] Attached. Kind: " + kind);});
 
 // debugging event
-// phidgets.on('line', function(line){ console.log("[line] " + line); });
+phidgets.on('line', function(line){ console.log("[line] " + line); });
 
 // connect
 phidgets.connect(function(){
@@ -22,5 +23,8 @@ phidgets.connect(function(){
 
   setInterval(function(){ phidgets.setOutput(null, 0, true); }, 1000);
   setInterval(function(){ phidgets.setOutput(null, 0, false);}, 1500);
+  setInterval(function(){ phidgets.setOutput(null, 0, true); }, 1000);
 
+
+  setInterval(function() { phidgets.quit(); }, 5000);
 });
