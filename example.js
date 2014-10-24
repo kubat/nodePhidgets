432bfa6ddfc7da1b2f0ced33b46a10a679a678f9
Rony Kubat <rony@kubat.org>
2014-10-23 23:27:22 -0400

    Attach and detatch events. Better handling of data from the web interface.

--- example.js
+++ example.js
@@ -11,20 +11,25 @@ phidgets.on('input',    function(boardId, id, value){ console.log("[" + boardId 
 phidgets.on('sensor',   function(boardId, id, value){ console.log("[" + boardId + "][sensor] " + id + " @ " + value); });
 phidgets.on('output',   function(boardId, id, value){ console.log("[" + boardId + "][output] " + id + " @ " + value); });
 phidgets.on('attached', function(boardId, kind){ console.log("[" + boardId + "] Attached. Kind: " + kind);});
+phidgets.on('detached', function(boardId, kind){ console.log("[" + boardId + "] Detached. Kind: " + kind);});
 
 // debugging event
 phidgets.on('line', function(line){ console.log("[line] " + line); });
 
+
+var dump = function() {
+    console.log("Dump: " + JSON.stringify(phidgets.data));
+    setTimeout(dump, 1000);
+}
+
+phidgets.on("attached", function(boardId, kind){
+    var value = true;
+    setInterval(function(){ phidgets.setOutput(boardId, 0, value); value = ! value}, 1000);
+})
+
 // connect
 phidgets.connect(function(){
-
-  console.log("connected to:");
-  console.log(phidgets.ids);
-
-  setInterval(function(){ phidgets.setOutput(null, 0, true); }, 1000);
-  setInterval(function(){ phidgets.setOutput(null, 0, false);}, 1500);
-  setInterval(function(){ phidgets.setOutput(null, 0, true); }, 1000);
-
-
-  setInterval(function() { phidgets.quit(); }, 5000);
+    console.log("connected to:", phidgets.ids);
+    dump();
+//    setTimeout(function() { phidgets.quit(); }, 5000);
 });
